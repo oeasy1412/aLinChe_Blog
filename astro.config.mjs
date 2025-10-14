ee62645ebc1e1e7c79c8c2e3e9438030afef8ce7
scutKKsix <1129332011@qq.com>
2025-10-14 22:35:04 +0800

    fix astroConfig URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://aLinChe.vercel.app/",
+	site: "https://blog.alinche.dpdns.org/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
